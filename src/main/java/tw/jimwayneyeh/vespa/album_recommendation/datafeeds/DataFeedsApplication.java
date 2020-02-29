76a647381a162b463313fb82840d7b3dd685e48b
Wayne Yeh <jimwayne@gmail.com>
2020-03-01 00:09:50 +0800

    Initiate with Spring initializer.

---
+++ src/main/java/tw/jimwayneyeh/vespa/album_recommendation/datafeeds/DataFeedsApplication.java
@@ -0,0 +1,11 @@
+package tw.jimwayneyeh.vespa.album_recommendation.datafeeds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataFeedsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DataFeedsApplication.class, args);
+	}
+}
---
+++ src/test/java/tw/jimwayneyeh/vespa/albumrecommendation/datafeeds/DataFeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package tw.jimwayneyeh.vespa.albumrecommendation.datafeeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataFeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
