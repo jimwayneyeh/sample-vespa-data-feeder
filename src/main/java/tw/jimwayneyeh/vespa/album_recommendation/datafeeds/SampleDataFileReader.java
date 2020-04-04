733d9d71912e835b4530f9594c48bec3c80eb804
Wayne Yeh <jimwayne@gmail.com>
2020-04-04 15:25:07 +0800

    Add OpenCSV for reading sample data.

--- src/main/java/tw/jimwayneyeh/vespa/album_recommendation/datafeeds/SampleDataFileReader.java
+++ src/main/java/tw/jimwayneyeh/vespa/album_recommendation/datafeeds/SampleDataFileReader.java
@@ -2,12 +2,13 @@ package tw.jimwayneyeh.vespa.album_recommendation.datafeeds;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import org.apache.commons.io.IOUtils;
+
+import com.opencsv.bean.CsvToBeanBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
 import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
@@ -19,16 +20,10 @@ public class SampleDataFileReader implements SampleDataReader {
 
   @Override
   public Flux<MusicAlbum> read() {
-    try (InputStream input = this.getClass().getResourceAsStream(fileName)) {
-      return Flux.fromIterable(IOUtils.readLines(input, StandardCharsets.UTF_8))
-          .filter(line -> !StringUtils.isEmpty(line))
-          .map(line -> {
-            String[] fileds = line.split(",");
-            return new MusicAlbum()
-                .artist(fileds[0])
-                .name(fileds[1])
-                .releaseYear(Integer.parseInt(fileds[2]));
-          });
+    try (InputStream input = this.getClass().getResourceAsStream(fileName);
+        InputStreamReader reader = new InputStreamReader(input, StandardCharsets.UTF_8)) {
+      return Flux.fromStream(
+              new CsvToBeanBuilder(reader).withType(MusicAlbum.class).build().stream());
     } catch (IOException ioe) {
       log.warn("Cannot read sample data from '{}'.", fileName, ioe);
       return Flux.empty();
